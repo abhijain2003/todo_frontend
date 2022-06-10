31569eed3b109734c8f87ad990b4026f595d6264
Abhi Jain <abhijain3002@gmail.com>
2022-06-10 16:34:17 +0530

    update made

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { GetAllAdvice } from "../Services/Tasks";
+import { GetAllAdvice, DeleteAdviceById, AddAdvice, EditAdviceData } from "../Services/Tasks";
 
 function Main() {
   //useState for storing all behaviour data
-  const [adviceData, setadviceData] = useState([])
+  const [adviceData, setadviceData] = useState([]);
+  const [newAdviceValue, setnewAdviceValue] = useState("");
+  const [updateAdviceValue, setupdateAdviceValue] = useState("");
 
   useEffect(() => {
     //getting behaviour from database from api 
     async function getAdvice() {
       await GetAllAdvice()
-        .then((res) =>
-          {setadviceData(res.data.data)}
+        .then((res) => { setadviceData(res.data.data) }
         )
         .catch((err) => console.log(err));
     }
@@ -19,9 +20,38 @@ function Main() {
     getAdvice()
   }, [])
 
+  async function handleDeleteAdvice(index) {
+    let id = adviceData[index]._id;
+    console.log(id);
+    await DeleteAdviceById({ id })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    window.location.reload();
+  }
+
+  async function handleAddAdvice() {
+    let data = {
+      advice: newAdviceValue
+    }
+    await AddAdvice({ data })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    window.location.reload();
+  }
+
+  async function handleUpdateAdvice(index){
+    await EditAdviceData({id: adviceData[index]._id, data: {
+      advice: updateAdviceValue
+    }}).then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+    window.location.reload();
+  }
+
   return (
     <div className="h-[100vh] overflow-y-hidden" >
-    {/* navbar */}
+      {/* navbar */}
       <div className="bg-[#FF5D5D] flex justify-evenly text-white font-bold text-[20px] h-[40px]">
         <p>ASSIGNMENT</p>
         <div className="flex">
@@ -34,16 +64,20 @@ function Main() {
         </div>
       </div>
 
-{/* all behaviour box */}
-      {adviceData.map((val, i) => (
-        <div className="grid grid-cols-4 h-full" >
-          <NavLink exact to={`/tasks/${val.advice1}`} ><div className="w-[250px] mt-16 h-[150px] mx-auto rounded-[5px] justify-center items-center flex" style={{ boxShadow: "rgba(255, 93, 93, 1) 0px 0px 0px 3px" }} key={i} >{val.advice1}</div></NavLink>
-          <NavLink exact to={`/tasks/${val.advice2}`} ><div className="w-[250px] mt-16 h-[150px] mx-auto rounded-[5px] justify-center items-center flex" style={{ boxShadow: "rgba(255, 93, 93, 1) 0px 0px 0px 3px" }} key={i} >{val.advice2}</div></NavLink>
-          <NavLink exact to={`/tasks/${val.advice3}`} ><div className="w-[250px] mt-16 h-[150px] mx-auto rounded-[5px] justify-center items-center flex" style={{ boxShadow: "rgba(255, 93, 93, 1) 0px 0px 0px 3px" }} key={i} >{val.advice3}</div></NavLink>
-          <NavLink exact to={`/tasks/${val.advice4}`} ><div className="w-[250px] mt-16 h-[150px] mx-auto rounded-[5px] justify-center items-center flex" style={{ boxShadow: "rgba(255, 93, 93, 1) 0px 0px 0px 3px" }} key={i} >{val.advice4}</div></NavLink>
-          <NavLink exact to={`/tasks/${val.advice5}`} ><div className="w-[250px] mt-16 h-[150px] mx-auto rounded-[5px] justify-center items-center flex" style={{ boxShadow: "rgba(255, 93, 93, 1) 0px 0px 0px 3px" }} key={i} >{val.advice5}</div></NavLink>
+      {/* all behaviour box */}
+      <div className="grid grid-cols-4 h-full">
+        {adviceData.map((val, i) => (
+          <div className="w-[250px] mt-16 h-[150px] mx-auto text-xl font-bold text-[#ff5d5d] rounded-[5px] justify-evenly items-center flex flex-col" style={{ boxShadow: "rgba(255, 93, 93, 1) 0px 0px 0px 3px" }} >
+            <div onClick={() => handleDeleteAdvice(i)} className="justify-end flex w-[100%]">🗑️</div>
+            <NavLink key={i} exact to={`/tasks/${val.advice}`} ><p className="capitalize" >{val.advice}</p></NavLink>
+            <div className="flex justify-center" ><input onChange={(e) => setupdateAdviceValue(e.target.value)} className="border-2 border-black w-[60%]" /><button onClick={() => handleUpdateAdvice(i)} className="bg-[#FF5D5D] text-[14px] py-auto ml-2 text-white h-[30px] my-auto rounded-[4px] px-[2px]" >ok</button></div>
+          </div>
+        ))}
+        <div className="w-[250px] mt-16 h-[150px] text-xl font-bold text-[#ff5d5d] mx-auto rounded-[5px] justify-evenly items-center flex flex-col" style={{ boxShadow: "rgba(255, 93, 93, 1) 0px 0px 0px 3px" }} >
+          <div className="flex justify-center" ><input onChange={(e) => setnewAdviceValue(e.target.value)} className="border-2 border-black w-[60%]" /></div>
+          <p onClick={handleAddAdvice} >Add+</p>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
